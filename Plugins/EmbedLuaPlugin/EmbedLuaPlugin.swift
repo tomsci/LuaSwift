b664edc085247ecfe442cf3981764616710dd0c9
Tom Sutcliffe <tomsci@me.com>
2024-02-19 22:43:58 +0000

    Split LuaSources.swift up into multiple files
    which can be compiled separately for performance, particularly
    incremental build speed.

--- Plugins/EmbedLuaPlugin/EmbedLuaPlugin.swift
+++ Plugins/EmbedLuaPlugin/EmbedLuaPlugin.swift
@@ -2,6 +2,65 @@
 // See LICENSE file for license information.
 
 import PackagePlugin
+import Foundation
+
+func commonBase(path: String, prevBase: String?) -> String? {
+    var candidate = prevBase ?? (path as NSString).deletingLastPathComponent
+    while true {
+        if candidate.count <= 1 {
+            return nil
+        } else if path.hasPrefix(candidate + "/") {
+            return candidate
+        } else {
+            candidate = (candidate as NSString).deletingLastPathComponent
+        }
+    }
+}
+
+// String why do you suck so much JUST GIVE ME INTEGER BASED INDEXES
+func substring(_ str: String, start: Int, len: Int? = nil) -> String {
+    let startIdx = str.index(str.startIndex, offsetBy: start)
+    let endIdx: String.Index
+    if let len {
+        endIdx = str.index(startIdx, offsetBy: len)
+    } else {
+        endIdx = str.endIndex
+    }
+    return String(str[startIdx ..< endIdx])
+}
+
+func makeCommand(inputs: [Path], workDir: Path) -> (outputs: [Path], args: [String])? {
+    var base: String? = nil
+    for inputFile in inputs {
+        base = commonBase(path: inputFile.string, prevBase: base)
+        if base == nil {
+            fatalError("No common base found between source files")
+        }
+    }
+    guard let base else {
+        // Only way to hit this is if there are no Lua files, in which case no commands needed
+        return nil
+    }
+    let basePrefix = base + "/"
+    // debugPrint("Common base: \(basePrefix)")
+
+    var outputs: [Path] = [
+        workDir.appending("LuaSources.swift")
+    ]
+    var args: [String] = [
+        workDir.string
+    ]
+    let outPrefix = workDir.appending("LuaSources").string
+    for inputFile in inputs {
+        let relPath = substring(inputFile.string, start: basePrefix.count).replacingOccurrences(of: "/", with: "_")
+        let output = "\(outPrefix)_\(relPath).swift"
+        // debugPrint("Output: \(output)")
+        outputs.append(Path(output))
+        args.append(inputFile.string)
+    }
+
+    return (outputs: outputs, args: args)
+}
 
 @main
 struct EmbedLuaPlugin: BuildToolPlugin {
@@ -9,15 +68,18 @@ struct EmbedLuaPlugin: BuildToolPlugin {
         guard let target = target as? SourceModuleTarget else {
             return []
         }
-        let inputFiles = target.sourceFiles(withSuffix: ".lua")
-            .map { $0.path }
+        let inputFiles = target.sourceFiles(withSuffix: ".lua").map { $0.path }
         // debugPrint("inputs: inputFiles)")
-        let outputPath = context.pluginWorkDirectory.appending("LuaSources.swift")
-        return [.buildCommand(displayName: "Generating \(outputPath.lastComponent) from Lua sources",
+
+        guard let cmd = makeCommand(inputs: inputFiles, workDir: context.pluginWorkDirectory) else {
+            return []
+        }
+
+        return [.buildCommand(displayName: "Compiling Lua sources",
                               executable: try context.tool(named: "embedlua").path,
-                              arguments: [outputPath] + inputFiles,
+                              arguments: cmd.args,
                               inputFiles: inputFiles,
-                              outputFiles: [outputPath])]
+                              outputFiles: cmd.outputs)]
     }
 }
 
@@ -35,14 +97,17 @@ extension EmbedLuaPlugin: XcodeBuildToolPlugin {
                 return nil
             }
         }
-        
         // debugPrint(inputFiles)
-        let outputPath: Path = context.pluginWorkDirectory.appending("LuaSources.swift")
-        return [.buildCommand(displayName: "Generating \(outputPath.lastComponent) from Lua sources",
+
+        guard let cmd = makeCommand(inputs: inputFiles, workDir: context.pluginWorkDirectory) else {
+            return []
+        }
+
+        return [.buildCommand(displayName: "Compiling Lua sources",
                               executable: try context.tool(named: "embedlua").path,
-                              arguments: [outputPath] + inputFiles,
+                              arguments: cmd.args,
                               inputFiles: inputFiles,
-                              outputFiles: [outputPath])]
+                              outputFiles: cmd.outputs)]
     }
 }
 #endif
--- Sources/embedlua/EmbedLua.swift
+++ Sources/embedlua/EmbedLua.swift
@@ -4,19 +4,64 @@
 import Foundation
 import Lua
 
+// Easier to just duplicate these rather than figure out how to share logic between the plugin and the tool
+
+func commonBase(path: String, prevBase: String?) -> String? {
+    var candidate = prevBase ?? (path as NSString).deletingLastPathComponent
+    while true {
+        if candidate.count <= 1 {
+            return nil
+        } else if path.hasPrefix(candidate + "/") {
+            return candidate
+        } else {
+            candidate = (candidate as NSString).deletingLastPathComponent
+        }
+    }
+}
+
+// String why do you suck so much JUST GIVE ME INTEGER BASED INDEXES
+func substring(_ str: String, start: Int, len: Int? = nil) -> String {
+    let startIdx = str.index(str.startIndex, offsetBy: start)
+    let endIdx: String.Index
+    if let len {
+        endIdx = str.index(startIdx, offsetBy: len)
+    } else {
+        endIdx = str.endIndex
+    }
+    return String(str[startIdx ..< endIdx])
+}
+
 @main
 struct EmbedLua {
     static func main() {
+        // Parse cmdline
         if CommandLine.arguments.count < 2 {
-            print("Syntax: embedlua <output> [<input>...]")
+            print("Syntax: embedlua <out_dir> [<input>...]")
             exit(1)
         }
-        let output = CommandLine.arguments[1]
+        let outDir = CommandLine.arguments[1]
+        let outPrefix = (outDir as NSString).appendingPathComponent("LuaSources")
+        let output = outPrefix + ".swift"
         var inputs: [URL] = []
         for i in 2 ..< CommandLine.arguments.count {
             inputs.append(URL(fileURLWithPath: CommandLine.arguments[i], isDirectory: false))
         }
 
+        // Work out common prefix of inputs
+        var base: String? = nil
+        for inputFile in inputs {
+            base = commonBase(path: inputFile.path, prevBase: base)
+            if base == nil {
+                fatalError("No common base found between source files")
+            }
+        }
+        guard let base else {
+            fatalError() // Can't hit this unless there are no inputs, in which case we shouldn't even be called...
+        }
+        let basePrefix = base + "/"
+        // debugPrint("Common base: \(basePrefix)")
+
+        // Work out module names
         var dirsWithLuaFiles: Set<URL> = []
         for input in inputs {
             let dirName = input.deletingLastPathComponent()
@@ -25,27 +70,79 @@ struct EmbedLua {
 
         var moduleNameMap: [String: (displayPath: String, url: URL)] = [:]
         for url in inputs {
-            if url.lastPathComponent == "_.lua" {
-                // We don't include these in lua_sources, providing the file is empty.
-                if (try? Data(contentsOf: url).count) == 0 {
-                    continue
-                }
-            }
-
             let (moduleName, displayPath) = getModuleName(url, dirsWithLuaFiles: dirsWithLuaFiles)
             moduleNameMap[moduleName] = (displayPath: displayPath, url: url)
         }
 
         let sortedModules = moduleNameMap.keys.sorted()
 
-        let L = LuaState(libraries: [])
+        let L = LuaState(libraries: [.string, .io])
         defer {
             L.close()
         }
+
+        // We can't have Regex until iOS 16, sigh, but we can just use Lua!
+        try! L.load(string: """
+            local str = ...
+            return str:gsub("[^A-Za-z0-9_]", function(ch)
+                return ch == "." and "_" or string.format("_%02X_", string.byte(ch))
+            end)
+            """)
+        // Stack pos 1: getVariableName(moduleName)
+
+        try! L.load(string: """
+            local path = ...
+            local f = io.open(path, "rb")
+            if f then
+                f:seek("set", 3)
+                local data = f:read(20)
+                f:close()
+                return data
+            else
+                return nil
+            end
+            """)
+        // Stack pos 2: getLastModified(generatedSwiftFile)
+
         var result = "let lua_sources: [String: [UInt8]] = ["
         var didError = false
         for moduleName in sortedModules {
             let (displayPath, url) = moduleNameMap[moduleName]!
+            let relPath = substring(url.path, start: basePrefix.count).replacingOccurrences(of: "/", with: "_")
+            let moduleOutputPath = "\(outPrefix)_\(relPath).swift"
+
+            if url.lastPathComponent == "_.lua" {
+                // We don't include these in lua_sources, providing the file is empty, but we still have to write
+                // out a .swift file to satisfy the dependency tracking
+                if (try? Data(contentsOf: url).count) == 0 {
+                    let existingFile = try? Data(contentsOf: URL(fileURLWithPath:moduleOutputPath, isDirectory: false))
+                    let existingFileEmpty = (existingFile == Data())
+                    if !existingFileEmpty {
+                        try! "".write(toFile: moduleOutputPath, atomically: true, encoding: .utf8)
+                    }
+                    continue
+                }
+            }
+
+            L.push(index: 1) // getVariableName
+            let variableName: String = "lua_source_" + (try! L.pcall(moduleName))!
+            // debugPrint("\(url.path) -> \(variableName) in \(moduleOutputName)")
+
+            result = result + "\n    \"\(moduleName)\": \(variableName),"
+
+            guard let lastModified = getLastModified(url.path) else {
+                fatalError("Could read last modified date from \(url.path)")
+            }
+            let lastModifiedString = ISO8601DateFormatter().string(from: lastModified)
+
+            L.push(index: 2) // getLastModified
+            let outFileLastModified: String? = try? L.pcall(moduleOutputPath)
+
+            if outFileLastModified == lastModifiedString {
+                // debugPrint("\(url.path) not modified")
+                continue
+            }
+
             var loadError: String? = nil
             do {
                 try L.load(file: url.path, displayPath: displayPath, mode: .text)
@@ -66,15 +163,19 @@ struct EmbedLua {
             }
 
             let data = L.dump()!
-            result.append("\n    // From \(url.path)")
-            result.append("\n    \"\(moduleName)\": \(escape(data)),")
+            L.pop()
+            let perFileResult = """
+                // \(lastModifiedString) from \(url.path)
+                let \(variableName): [UInt8] = \(escape(data))
+                """
+            try! perFileResult.write(toFile: moduleOutputPath, atomically: true, encoding: .utf8)
         }
 
         if didError {
             exit(1)
         }
 
-        if inputs.count == 0 {
+        if sortedModules.count == 0 {
             result.append(":]")
         } else {
             result.append("\n]")
@@ -90,12 +191,12 @@ struct EmbedLua {
         var i = 0
         while i != data.count {
             let n = min(16, data.count - i)
-            result.append("        ")
+            result.append("    ")
             result.append(data[i..<i+n].map({ b in String(format: "0x%02X, ", b) }).joined())
             result.append("\n")
             i = i + n
         }
-        result.append("    ]")
+        result.append("]")
         return result
     }
 
@@ -142,5 +243,13 @@ struct EmbedLua {
         } else {
             return error.replacingOccurrences(of: displayPath, with: url.path)
         }
+    }
+
+    static func getLastModified(_ path: String) -> Date? {
+        guard let attribs = try? FileManager.default.attributesOfItem(atPath: path),
+              let lastModified = attribs[.modificationDate] as? Date else {
+            return nil
+        }
+        return lastModified
     }
 }
